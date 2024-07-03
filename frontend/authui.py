02436a58e4160a584dd9ed1308456663a6e06bae
spectropaws <spectropaws777@gmail.com>
2024-07-03 10:43:56 +0530

    Initial commit

---
+++ frontend/authui.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+app = Tk()
+app.title("Authentication")
+app.geometry("500x400")
+
+label = Label(app, text="Login", font=("Arial", 24))
+label.pack(side=TOP, pady=40)
+
+username_frame = Frame(app)
+username_frame.pack(pady=(40, 10))
+Label(username_frame, text="Username:", font=("Arial", 16)).pack(side=LEFT, padx=20)
+username_field = Entry(username_frame, font=("Arial", 16))
+username_field.pack(padx=20)
+
+password_frame = Frame(app)
+password_frame.pack(pady=(40, 10))
+Label(password_frame, text="Password:", font=("Arial", 16)).pack(side=LEFT, padx=20)
+password_field = Entry(password_frame, font=("Arial", 16))
+password_field.pack(padx=20)
+
+submit_button = Button(app, text="Login", font=("Arial", 16), compound=CENTER, fg="white", bg="blue", height=2, width=10, padx=15, pady=5, border=0)
+submit_button.pack(pady=20)
+
+app.mainloop()
